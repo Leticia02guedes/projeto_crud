d483b93a9286d2be657b19f791f6ed84b251b05b
Leticia Guedes <leticia.guedes4@aluno.ce.gov.br>
2025-04-29 11:42:29 -0300

    Estilização da tabela

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
-
 function App() {
-  return (
-   <div className="App">
-    <Formulario/>
-    <Tabela/>
-   </div>
-  );
+
+const[btnCadastrar] =useState(true);
+const[produtos, setProdutos] =useState([]);
+
+
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+
+return (    
+      <div>        
+        <Formulario botao={btnCadastrar}/>
+        <Tabela vetor={produtos}/>
+      </div>
+  )
 }
 
 export default App;
--- frontend/front/src/Formulario.js
+++ frontend/front/src/Formulario.js
@@ -1,15 +1,20 @@
-function Formulario(){
-    return (
+function Formulario({botao}){
+    return(
         <form>
-            <h1>Formulário</h1>
-
-            <input type="text" placeholder="Nome"/>
-            <input type="text" placeholder="Marca"/>
-
-            <input type="button" value="Cadastrar"/>
-            <input type="button" value="Alterar"/>
-            <input type="button" value="Remover"/>
-            <input type="button" value="Cancelar"/>
+            <h1>Formulario</h1>
+            <input className="form-control" type="text" placeholder="Nome"></input>
+            <input className="form-control" type="text" placeholder="Marca"></input>
+            {
+               botao
+                ?
+                <input className="btn btn-success" type="button" value="Cadastrar" />
+                :
+                <div>
+                    <input className="btn btn-warning" type="button" value="Cancelar" />
+                    <input className="btn btn-primary" type="button" value="Alterar" />
+                    <input className="btn btn-danger"  type="button" value="Remover" />
+                </div>
+            }
         </form>
     )
 }
